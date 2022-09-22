d06711c7abcd04d565b580115325af5e3f5e1b14
Mehul-Suryavanshi <mehulsuryawanshi9@gmail.com>
2022-09-22 22:54:04 +0530

    Minor CSS Changes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,26 @@
-var btn = document.querySelector("button");
-var inputTextArea = document.querySelector('#input-textarea');
-var  apiEndPoint = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=';
-var requestString = '';
-var translatedString = '';
+var btn = document.querySelector("#btn-input");
+var inputTextArea = document.querySelector("#input-textarea");
+var outputTextArea = document.querySelector("#output-textarea");
+var apiEndPoint =
+  "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+var requestString = "";
 
+btn.addEventListener("click", () => {
+  requestString = apiEndPoint + inputTextArea.value;
+  console.log("Request->" + requestString);
+  callTranslateApi();
+});
 
-btn.addEventListener('click',() => {
-
-    requestString = apiEndPoint + inputTextArea.value;
-    console.log('Request->'+requestString);
-    callTranslateApi();
-})
-
-function callTranslateApi(){
-    fetch(requestString).then(response => {
-        console.log('Reponse->'+ response.json());
-        return response;
-    }).then(jsonParse => {
-        console.log('Translated Text->' + jsonParse.contents.translated)
-    }).catch(error => {
-        console.log('Error Occured : '+error);
+function callTranslateApi() {
+  fetch(requestString)
+    .then(response => response.json())
+    .then((jsonParse) => {
+      let translatedTest = jsonParse.contents.translated;
+      outputTextArea.innerHTML = translatedTest;
+      console.log("Translated Text->" + translatedTest);
     })
+    .catch((error) => {
+      alert("Oop's we hit a snag!! Please try again later");
+      console.log(error);
+    });
 }
